8d14211bd4229220ad6194eb5b0f2c58c2866459
enohyn <shayokullah@gmail.com>
2023-08-23 15:47:26 +0600

    store js file re-factored

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productSlice from "./slice/productSlice";
 import singleProductSlice from "./slice/singleProductSlice";
+
 const store = configureStore({
     reducer: {
         products: productSlice,
